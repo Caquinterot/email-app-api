b3d1c4dd990cdef06d3c5169ad8baa28e7540dca
cesarQuintero <131917778+Caquinterot@users.noreply.github.com>
2025-11-12 00:30:25 -0500

    modificacion del archivo connecdb

--- src/db/connecdb.js
+++ src/db/connecdb.js
@@ -1,15 +1,10 @@
 import { Sequelize } from 'sequelize';
 
-const sequelize = new Sequelize(
-	process.env.DB_NAME,
-	process.env.DB_USER,
-	process.env.DB_PASS || undefined,
-	{
-		host: process.env.DB_HOST,
-		port: Number(process.env.DB_PORT || 5432),
-		dialect: 'postgres',
-		logging: false,
-	},
-);
+console.log(process.env.DATABASE_URL);
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+	dialect: 'postgres',
+	logging: false,
+});
 
 export default sequelize;
