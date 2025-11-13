56174df2c4d8873a310117fbc2b89868fa7588f3
cesarQuintero <131917778+Caquinterot@users.noreply.github.com>
2025-11-12 23:18:31 -0500

    Init 5

--- src/utils/emailService.js
+++ src/utils/emailService.js
@@ -11,7 +11,7 @@ const transporter = nodemailer.createTransport({
 	},
 	tls: {
 		//Evita "self-signed certificate in certificate chain" en dev
-		rejectUnauthorized: true,
+		rejectUnauthorized: false,
 	},
 });
 
